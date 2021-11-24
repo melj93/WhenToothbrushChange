72a5ff5456d9a7e7044e2cb1609ef3ff66b9e433
Mel <melj@kakao.com>
2021-11-24 16:15:37 +0900

    Initial Commit

---
+++ WhenToothbrushChange/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WhenToothbrushChange
+//
+//  Created by samuel Jeong on 2021/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var subtitleSentence: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func btnCenter(_ sender: UIButton) {
+    }
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+}
